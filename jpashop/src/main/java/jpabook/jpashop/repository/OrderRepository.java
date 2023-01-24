fbc841fa1e4e10a159b7b89f696532c264d0ca2d
seunghyeon <sansusjc3@naver.com>
2023-01-25 04:15:57 +0900

    주문 관련

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,5 +20,6 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+//    public List<Order> findAll(OrderSearch orderSearch) {} 추후 예정
+
 }
