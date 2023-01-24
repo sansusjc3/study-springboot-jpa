4b6fd72bda65ac749157226c7baa3e13b0119508
seunghyeon <sansusjc3@naver.com>
2023-01-25 04:14:16 +0900

    주문

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.service;
+
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // final 붙은 필드 인젝션
+public class MemberService {
+
+    // 건드릴 일 없기 때문에 final 넣는 것 추천
+    private final MemberRepository memberRepository;
+
+    // 생성자가 하나인 경우 자동으로 인젝션 해줌
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //Exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
