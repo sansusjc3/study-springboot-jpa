2f2cfa7620a1e9ad8b85fa57d40009b77d31ca95
조승현 <sansusjc3@naver.com>
2023-01-25 16:52:11 +0900

    마지막 챕터

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id@GeneratedValue
     @Column(name = "order_item_id")
@@ -20,6 +23,8 @@ public class OrderItem {
     private Order order;
     private int orderPrice; //주문가격
     private int count; //주문 수량
+
+
 
     //== 생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
