99abab18628fa91e7a5991f6da658126ddc9c05d
seunghyeon <sansusjc3@naver.com>
2023-01-29 16:42:50 +0900

    완강 맨

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,18 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+
+    // 조금 귀찮더라도 merge가 아닌 변경감지를 통해 하나하나 해야한다.
+    // merge는 다 업데이트 해버리기 때문에 변경 폼에서 입력하지 않은 값은 null로 업데이트 됨.
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity)
+    {
+        Item findItem = itemRepository.findOne(id);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
     public List<Item> findItems() {
         return itemRepository.findAll();
